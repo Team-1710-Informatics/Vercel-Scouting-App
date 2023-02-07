ad6c4fb73f7a58a6b79c48e3946aa298e1848f60
Team-1710-Informatics <team1710.io@gmail.com>
2023-02-07 11:22:06 -0600

    ????

--- src/routes/(authorized)/scout/2023/match/Node.svelte
+++ src/routes/(authorized)/scout/2023/match/Node.svelte
@@ -6,8 +6,24 @@
     //@ts-ignore
     import slide from 'svelte-slidediag-transition';
     import { getIntake, deleteIntake } from './Inventory.svelte';
-    //@ts-ignore
-    import clickOutside from 'svelte-outside-click';
+
+    function clickOutside(node:any, onEventFunction:any) {
+        const handleClick = (event:any) => {
+            var path = event.composedPath();
+
+            if (!path.includes(node)) {
+                onEventFunction();
+            }
+        }
+
+        document.addEventListener("click", handleClick);
+
+        return {
+            destroy() {
+                document.removeEventListener("click", handleClick);
+            }
+        }
+    }
 
     const imgs = {
         cube:cube,
