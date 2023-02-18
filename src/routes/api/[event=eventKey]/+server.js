a408e2818b900dc9b58bc8b4b00001bcd712da74
Micah Lehman <micah.r.lehman@gmail.com>
2023-02-18 09:09:45 -0600

    e

--- src/routes/api/[event=eventKey]/+server.js
+++ src/routes/api/[event=eventKey]/+server.js
@@ -1,4 +1,17 @@
 import { MongoClient } from 'mongodb';
 import { fail, redirect } from '@sveltejs/kit';
+import { json } from '@sveltejs/kit';
 
-const client = new MongoClient(MONGODB);
+// const client = new MongoClient(MONGODB);
+
+export async function GET({ params }) {
+    // await client.connect();
+    // const jason = await client.db("main").collection("rawdata").findOne({ event:params.event });
+    const jason = {
+        name: "Jason",
+        hairstyle: "bread",
+        outfit: "none",
+        event: "2023test"
+    }
+    return json(jason);
+}
--- src/routes/apidocs/+page.svelte
+++ src/routes/apidocs/+page.svelte
@@ -1,8 +1,11 @@
 <script>
-
 </script>
 
+<!-- <svelte:head><p class="bg-blue-500">API Routes</p></svelte:head> -->
 
+<middle>
+    <!--  -->
+</middle>
 
 <style>
 
