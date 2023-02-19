3976f71faf5ca135684b2ddd8fce6c75cbaf3127
Gaven <team1710.io@gmail.com>
2023-02-18 22:34:20 -0600

    Install prompt?

--- src/routes/(authorized)/hub/+page.svelte
+++ src/routes/(authorized)/hub/+page.svelte
@@ -1,4 +1,6 @@
 <script lang=ts>
+    import { slide } from "svelte/transition";
+    import { onMount } from "svelte/types/runtime/internal/lifecycle";
     import CompetitionTracker from "./CompetitionTracker.svelte";
 
     export let data:any;
@@ -6,13 +8,23 @@
     const links:[string, string, number, string?][] = [
         ["Leaderboard", '/leaderboard', 4, ''],
         ["Data", '/data', 2, 'border-orange-600 bg-gradient-to-bl from-orange-500 to-yellow-300'],
-        ["Scout", '/scout/2023/pre', 6, 'submit'],
-        ["Simon Cowell", '/simon', 6, ''],
+        ["Scout", '/scout/2023/pre', 6, 'submit']
     ]
 
     if(data.permissions.includes("admin") || data.user == "Brooks"){
         links.unshift(["Admin", "/admin", 6, "border-red-600 bg-gradient-to-br from-rose-800 to-pink-600"])
     }
+
+    let deferredPrompt:any;
+
+    onMount(()=>{
+        window.addEventListener('beforeinstallprompt', (e) => {
+            // Prevent the mini-infobar from appearing on mobile
+            e.preventDefault();
+            // Stash the event so it can be triggered later.
+            deferredPrompt = e;
+        });
+    })
 </script>
 
 <svelte:head>
@@ -28,4 +40,7 @@
             <a href={link[1]} class="w-full font-bold" style="grid-column: span {link[2]} / span {link[2]};"><button class={link?.[3]+" w-full py-3"}>{link[0]}</button></a>
         {/each}
     </div>
+    {#if deferredPrompt}
+        <button class="my-5" transition:slide on:click={deferredPrompt?.prompt}>Install App</button>
+    {/if}
 </middle>
