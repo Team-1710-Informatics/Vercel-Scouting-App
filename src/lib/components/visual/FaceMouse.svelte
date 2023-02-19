10ee40369608e3d5a57d320173e4ad00d37fccc3
Gaven <team1710.io@gmail.com>
2023-02-18 22:06:17 -0600

    PWA yayy

--- src/lib/components/visual/FaceMouse.svelte
+++ src/lib/components/visual/FaceMouse.svelte
@@ -1,21 +1,23 @@
 <script>
-    import { onMount } from "svelte";
-
-
-    export let constrain = 500;
+    export let constrain = 1000;
     let content;
-    let xr;
-    let yr;
+
+    let style = "";
+
+    let ww;
+    let wh;
     
     function move(e){
+        if(wh > ww) return;
         let box = content.getBoundingClientRect();
-        xr = -(e.clientY - box.y - (box.height / 2)) / constrain;
-        yr = (e.clientX - box.x - (box.width / 2)) / constrain;
+        let xr = -(e.clientY - box.y - (box.height / 2)) / constrain;
+        let yr = (e.clientX - box.x - (box.width / 2)) / constrain;
+        style = `transform:perspective(100px) rotateX(${xr}deg) rotateY(${yr}deg)`;
     }
 </script>
 
-<svelte:window on:mousemove={move} />
+<svelte:window on:mousemove={move} bind:innerHeight={wh} bind:innerWidth={ww}/>
 
-<div bind:this={content} class="w-fit" style="transform:perspective(100px) rotateX({xr}deg) rotateY({yr}deg)">
+<div bind:this={content} class="w-fit" {style}>
     <slot />
 </div>
--- src/routes/(authorized)/hub/+page.svelte
+++ src/routes/(authorized)/hub/+page.svelte
@@ -4,8 +4,8 @@
     export let data:any;
 
     const links:[string, string, number, string?][] = [
-        ["Leaderboard", '/leaderboard', 3, ''],
-        ["Data", '/data/ranking', 3, 'submit'],
+        ["Leaderboard", '/leaderboard', 4, ''],
+        ["Data", '/data', 2, 'border-orange-600 bg-gradient-to-bl from-orange-500 to-yellow-300'],
         ["Scout", '/scout/2023/pre', 6, 'submit']
     ]
 
@@ -22,10 +22,9 @@
     <div class="my-10 box">
         <CompetitionTracker events={data.events} />
     </div>
-    <div class="grid grid-cols-6 w-60">
+    <div class="grid grid-cols-6 w-60 gap-2">
         {#each links as link}
-            <a href={link[1]} class="m-2 w-full" style="grid-column: span {link[2]} / span {link[2]};"><button class={link?.[3]+" w-full py-3"}>{link[0]}</button></a>
+            <a href={link[1]} class="w-full font-bold" style="grid-column: span {link[2]} / span {link[2]};"><button class={link?.[3]+" w-full py-3"}>{link[0]}</button></a>
         {/each}
     </div>
 </middle>
-
