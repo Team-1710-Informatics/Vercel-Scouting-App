f40bfc558359285c7d2e2be3622706691689bc5f
Team-1710-Informatics <team1710.io@gmail.com>
2023-03-03 16:21:28 -0600

    Credit awards

--- src/routes/(authorized)/scout/2023/+page.server.js
+++ src/routes/(authorized)/scout/2023/+page.server.js
@@ -1,5 +1,6 @@
 import { X_TBA_AUTHKEY } from "$env/static/private";
 import { ScoutData } from "$lib/models";
+import credits from "$lib/server/credits";
 import { redirect } from "@sveltejs/kit";
 
 export async function load({ locals }) {
@@ -21,9 +22,12 @@ export async function load({ locals }) {
 }
 
 export const actions = {
-    default: async function({ request }){
+    default: async function({ request, locals }){
         const input = await request.formData();
         const data = JSON.parse(input.get("data"));
+
+        if(locals.competition == data.event) await credits.transaction(locals.user.username, 100, `Scouted ${data.event}qm${data.match}:${data.team}`);
+        else await credits.transaction(locals.user.username, 20, `Scouted ${data.event}qm${data.match}:${data.team} (extra)`);
 
         const db = new ScoutData(data);
         await db.save();
