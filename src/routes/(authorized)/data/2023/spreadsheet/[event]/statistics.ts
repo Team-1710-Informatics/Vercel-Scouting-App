91a02c4e122e9640a0b6f3b93a2d0719f2d52ff8
Team-1710-Informatics <team1710.io@gmail.com>
2023-03-17 12:47:17 -0500

    Fix score calculation

--- src/routes/(authorized)/data/2023/spreadsheet/[event]/statistics.ts
+++ src/routes/(authorized)/data/2023/spreadsheet/[event]/statistics.ts
@@ -110,17 +110,17 @@ function teamScore(e:any){
     e.game.actions.forEach((a:any)=>{
         if(a.action === 'place') {
             switch(a.node.y){
-                case 0: count+= 5;
-                case 1: count+= 3;
-                case 2: count+= 2;
+                case 0: count+= 5; break;
+                case 1: count+= 3; break;
+                case 2: count+= 2; break;
             }
             if(a.time - e.game.start <= 18000) count++;
         }
         if(a.action === 'intake' && typeof a.location != "string"){
             switch(a.location.y){
-                case 0: count-= 5;
-                case 1: count-= 3;
-                case 2: count-= 2;
+                case 0: count-= 5; break;
+                case 1: count-= 3; break;
+                case 2: count-= 2; break;
             }
         }
     });
