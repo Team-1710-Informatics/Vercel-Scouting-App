d1b723d00af1f83047b18d3aa7f52244280733a6
Team-1710-Informatics <team1710.io@gmail.com>
2023-03-22 15:31:37 -0500

    managable text size

--- src/routes/(authorized)/(fun)/logofy/+page.svelte
+++ src/routes/(authorized)/(fun)/logofy/+page.svelte
@@ -1,10 +1,11 @@
 <script>
     import Logofy from "$lib/components/visual/Logofy/Logofy.svelte";
 
+    let width;
 </script>
 
-<middle class="p-10 w-fit">
-    <div class="mx-auto">
-        <Logofy size={60} contenteditable>1710</Logofy>
-    </div>
-</middle>
+<svelte:window bind:innerWidth={width}/>
+
+<div class="py-10 px-2">
+    <Logofy size={32} contenteditable>1710</Logofy>
+</div>
