4c60ac7759aa9048d237eb5f3b47b25a10c34bc7
Team-1710-Informatics <team1710.io@gmail.com>
2023-03-23 22:08:23 -0500

    gitignore?

--- src/routes/(authorized)/(fun)/scamble/bets/Bet.svelte
+++ src/routes/(authorized)/(fun)/scamble/bets/Bet.svelte
@@ -3,7 +3,14 @@
     import { page } from "$app/stores";
     import { enhance } from "$app/forms";
 
-    const MAX_BET=0.25;
+    function max(c){
+        if(c <= 400){
+            return Math.trunc(c/4)
+        }else if(c <= 500){
+            return Math.trunc(c-(c-400)/4)
+        }
+        return c-100;
+    };
 
     export let data;
     export let match;
@@ -37,12 +44,12 @@
                 <div class="w-fit text-2xl self-center text-teal-500"><Credits class="">{credits-wager}</Credits> credits</div>
                 <input type="number" name="wager" bind:value={wager}/>
                 <input hidden type="text" name="match" bind:value={match.key}/>
-                <p class="opacity-50 text-xs">Max bet: {Math.trunc(credits*MAX_BET)}</p>
+                <p class="opacity-50 text-xs">Max bet: {max(credits)}</p>
                 <div class="grid grid-cols-2 my-2">
                     <label class="text-blue-400 accent-blue-900"><input bind:group={alliance} type="radio" name="alliance" value="blue"/> Blue</label>
                     <label class="text-red-400 accent-red-900"><input bind:group={alliance} type="radio" name="alliance" value="red"/> Red</label>
                 </div>
-                <button class="bg-gradient-to-t from-slate-800 to-slate-500 border-black" disabled={wager > credits*MAX_BET || wager <= 0 || wager != Math.trunc(wager) || loading }>Bet <Credits>{wager}</Credits> credits</button>
+                <button class="bg-gradient-to-t from-slate-800 to-slate-500 border-black" disabled={wager > max(credits) || wager <= 0 || wager != Math.trunc(wager) || loading }>Bet <Credits>{wager}</Credits> credits</button>
             </form>
         {/await}
     </center>
