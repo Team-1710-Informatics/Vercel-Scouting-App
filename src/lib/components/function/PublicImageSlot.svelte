24902ce9f067f5c308c1d50beddd167af040a129
Team-1710-Informatics <team1710.io@gmail.com>
2023-03-30 08:57:43 -0500

    perhaps

--- src/lib/components/function/PublicImageSlot.svelte
+++ src/lib/components/function/PublicImageSlot.svelte
@@ -1,13 +1,12 @@
 <script>
     import { enhance } from "$app/forms";
-    import { PUBLIC_HOST } from "$env/static/public";
     export let path;
     export let name;
 
     let state = "Upload";
 </script>
 
-<form enctype="multipart/form-data" method=POST action="{PUBLIC_HOST}/internal/upload" use:enhance={() => {
+<form enctype="multipart/form-data" method=POST action="https://team1710.com/scouting/files/upload.php" use:enhance={() => {
     state = "Loading..."
     //@ts-ignore
     return async ({ update }) => {
