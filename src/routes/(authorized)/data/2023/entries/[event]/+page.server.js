5eea624babd2566712aee18bae178dd9e2cbeb4e
GavenB141 <gzbehrends@gmail.com>
2023-03-30 13:17:26 -0500

    Update +page.server.js

--- src/routes/(authorized)/data/2023/entries/[event]/+page.server.js
+++ src/routes/(authorized)/data/2023/entries/[event]/+page.server.js
@@ -3,5 +3,10 @@ import tba from "$lib/modules/tba";
 export async function load({params}){
     const events = await tba(`events/2023`);
 
+    events.push {
+        short_name: "Practice"
+        key: "2023practice"
+    }
+
     return { events, competition:params.event };
 }
