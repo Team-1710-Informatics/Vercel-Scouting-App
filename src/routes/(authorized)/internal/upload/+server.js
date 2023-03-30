76f0bf03069989958d53eee5c065cd901a01fd9b
Team-1710-Informatics <team1710.io@gmail.com>
2023-03-30 09:56:09 -0500

    perhaps

--- src/routes/(authorized)/internal/upload/+server.js
+++ src/routes/(authorized)/internal/upload/+server.js
@@ -1,7 +1,7 @@
 import { json } from "@sveltejs/kit";
 
 export async function POST({ request }){
-    request.headers = { "Origin":"https://team1710.com" };
+    request.headers.append("Origin", "https://team1710.com");
 
     let res = await fetch("https://team1710.com/scouting/files/upload.php", request);
 
