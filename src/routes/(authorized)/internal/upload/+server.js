d9a994c36374541e8c742b03545ff10a40e17d10
Team-1710-Informatics <team1710.io@gmail.com>
2023-03-30 09:00:36 -0500

    perhaps

--- src/routes/(authorized)/internal/upload/+server.js
+++ src/routes/(authorized)/internal/upload/+server.js
@@ -2,5 +2,7 @@ import {json} from "@sveltejs/kit";
 
 export async function POST({request, fetch}){
     request.headers.append('Access-Control-Allow-Origin', `https://team1710.com`);
-    return json(await fetch("https://team1710.com/scouting/files/upload.php", request));
+    return json(await fetch("https://team1710.com/scouting/files/upload.php", {
+        headers:request.headers
+    }));
 }
