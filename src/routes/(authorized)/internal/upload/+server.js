805cd3375b47434eed8b2d65b6c68dd5a08f46c7
Team-1710-Informatics <team1710.io@gmail.com>
2023-03-30 10:49:25 -0500

    perhaps

--- src/routes/(authorized)/internal/upload/+server.js
+++ src/routes/(authorized)/internal/upload/+server.js
@@ -3,10 +3,18 @@ import { json } from "@sveltejs/kit";
 export async function POST({ request }){
     // request.headers.append("Origin", "https://team1710.com");
 
-    let res = {};
-    try{
-        res = await fetch("https://team1710.com/scouting/files/upload.php", request);
-    }catch(e){console.log(e)}
+    // const req = await request.formData();
+    // const file = request.get("upload");
+    // const name = request.get("")
+
+    const res = await fetch("https://team1710.com/scouting/files/upload.php", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+            "Content-Type": "multipart/form-data",
+        },
+        body: request.body
+    })
 
     return json(res);
 }
