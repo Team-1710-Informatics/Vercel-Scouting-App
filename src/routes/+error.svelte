034e75304290a7e51aa0722f9e513117d4756bd4
Team-1710-Informatics <team1710.io@gmail.com>
2023-03-31 09:37:48 -0500

    Error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { page } from "$app/stores";
+</script>
+
+<middle>
+    <h6>{$page.status} {$page.error.message}</h6>
+    {#if $page.status == 405}
+        <p>Most likely a service issue, just refresh</p>
+    {/if}
+    <button class="p-2" on:click={document.location.reload}>Try again</button>
+</middle>
