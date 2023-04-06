c31eae49f3c44e50861f8184f341d7e6afea1d38
Micah Lehman <micah.r.lehman@gmail.com>
2023-04-06 16:26:56 -0500

    apidocs additions

--- src/routes/(authorized)/apidocs/+page.svelte
+++ src/routes/(authorized)/apidocs/+page.svelte
@@ -4,28 +4,46 @@
     import eventTeam from "../../(api)/api/[event=eventKey]/frc[team]/info.js";
     import match from "../../(api)/api/[event=eventKey]/[match]/info.js";
     import matchTeam from "../../(api)/api/[event=eventKey]/[match]/frc[team]/info.js";
+    import arrow from "$lib/assets/icons/caret.svg";
+    import { slide } from 'svelte/transition';
 
-    const routes = [
-        teams,
+    const eventRoutes = [
         event,
         eventTeam,
         match,
         matchTeam,
     ]
+    const teamRoutes = [
+        teams,
+    ]
 </script>
 <center>
 <div>
-    <h1>Scouting API</h1>
+    <span style="font-size:xx-large;"><u>Scouting API</u></span>
     <p>
         The scouting API allows access to viewing of scouting data.
     </p>
-    {#each routes as route}
+    <h1>Event</h1>
+    {#each eventRoutes as route}
+        <button on:click={route.checked=!route?.checked}>
+            <div class="event-route"> 
+            <img src={arrow} style='align-items: center; {route?.checked?"":"transform:rotate(-90deg);"}' alt="">
+                {route.url}
+            </div>
+        </button>
+        {#if route?.checked}
+            this route
+        {/if}
+        <br>
+    {/each}
+    <h1>Team</h1>
+    {#each teamRoutes as route}
         <button>
-            <img src="$lib/assets/icons/arrowDown.svg" alt="">
-            <span class="route"> 
+            <img src={arrow} alt="">
+            <div class="team-route"> 
                 <!-- {condition?if_true:if_false} -->
                 {route.url}
-            </span>
+            </div>
         </button>
         <br>
     {/each}
@@ -33,9 +51,20 @@
 </center>
 
 <style>
-    .route{
+    .event-route{
         border-radius: 3px;
         border: 3px white;
         width: 80vw;
+        font-size:x-large;
+        font-weight:700;
+        background-color:indigo;
+    }
+    .team-route{
+        border-radius: 3px;
+        border: 3px white;
+        width: 80vw;
+        font-size:x-large;
+        font-weight:700;
+        background-color:black;
     }
 </style>
