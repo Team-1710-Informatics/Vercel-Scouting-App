539ffdfbfe081d8d54dd06df9591568781a1c368
Micah Lehman <micah.r.lehman@gmail.com>
2024-02-05 17:34:21 -0600

    start of merge

--- src/routes/(authorized)/scout/2023/+page.server.js
+++ src/routes/(authorized)/scout/2023/+page.server.js
@@ -7,7 +7,7 @@ import { redirect } from "@sveltejs/kit";
 export async function load({ locals, url }) {
     if(!locals.user) throw redirect(307, "/login");
 
-    const events = await tba(`events/2024`);
+    const events = await tba(`events/2023`);
 
     return{
         events,
