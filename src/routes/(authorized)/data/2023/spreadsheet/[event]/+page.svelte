4a48585389de9037b53becb53f9537badf98d203
Sam <finepointdog@gmail.com>
2024-02-10 11:25:40 -0600

    semi-broken pagination

--- src/routes/(authorized)/data/2023/spreadsheet/[event]/+page.svelte
+++ src/routes/(authorized)/data/2023/spreadsheet/[event]/+page.svelte
@@ -3,6 +3,11 @@
     import stats from "./statistics"
     import Plot from 'svelte-plotly.js';
     import { onMount } from 'svelte';
+    import Pagination from '$lib/components/ui/Pagination.svelte'
+
+    const examples = ['The Pudding is a digital publication that explains ideas debated in culture with visual essays.'.split(' '), 'The Pudding is a digital publication that explains ideas debated in culture with visual essays.'.split(' ')]
+
+    let values;
 
     let teams = [];
     let first = 1;
@@ -104,6 +109,8 @@
         return ((+stats[sortFunction](b,data.entries.filter(matfil)))-(+stats[sortFunction](a,data.entries.filter(matfil))))*(ascending? -1 : 1);
     })
 
+    $: filtered_teams = []
+
     function add_column() {
         columns.push("Average_score");
         update_graph_data()
@@ -118,10 +125,13 @@
 
     $: graph_data = [{'x': [], 'y': [], type: 'scatter', mode: 'markers'},
                     {'x': [], 'y': [], type: 'line'}];
+        $: new_data = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    $: filtered_data = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
     function update_graph_data() {
         graph_data[0]['x'] = []
         graph_data[0]['y'] = []
+
         for (const team of teams) {
             graph_data[0]['x'].push((typeof stats[columns[0]](team, data.entries.filter(matfil)) === "number" && stats[columns[0]](team, data.entries.filter(matfil)) != Math.trunc(stats[columns[0]](team, data.entries.filter(matfil)))) ? parseFloat(stats[columns[0]](team, data.entries.filter(matfil))).toFixed(2) : stats[columns[0]](team, data.entries.filter(matfil)))
             graph_data[0]['y'].push((typeof stats[columns[1]](team, data.entries.filter(matfil)) === "number" && stats[columns[1]](team, data.entries.filter(matfil)) != Math.trunc(stats[columns[1]](team, data.entries.filter(matfil)))) ? parseFloat(stats[columns[1]](team, data.entries.filter(matfil))).toFixed(2) : stats[columns[1]](team, data.entries.filter(matfil)))
@@ -132,127 +142,164 @@
         graph_data[1]['x'] = movingAvg(graph_data[0]['x'], 10)
         graph_data[1]['y'] = movingAvg(graph_data[0]['y'], 10)
 
-        console.log(graph_data)
+        generate_new_data()
     }
+
+    function generate_new_data() {
+        new_data = []
+        let i = 0
+        console.log(teams)
+        for (const column in columns) {
+            new_data.push([])
+            for (const team of teams) {
+                new_data[i].push((typeof stats[columns[column]](team, data.entries.filter(matfil)) === "number" && stats[columns[column]](team, data.entries.filter(matfil)) != Math.trunc(stats[columns[column]](team, data.entries.filter(matfil)))) ? parseFloat(stats[columns[column]](team, data.entries.filter(matfil))).toFixed(2) : stats[columns[column]](team, data.entries.filter(matfil)))
+            }
+            i++;
+        }
+
+        new_data=new_data;
+        console.log(new_data)
+    }
+    generate_new_data()
 
     onMount(async () => {
         update_graph_data()
     })
 
     let showGraph = false;
-
 </script>
 
-<center class="pt-10">
-    <div class="grid grid-cols-2 w-fit gap-1">
-        <button class="font-bold bg-gradient-to-t from-green-800 to-green-400 border-green-900" on:click={add_column}>Add column</button>
-        <button class="font-bold bg-gradient-to-t from-red-800 to-red-400 border-red-900" on:click={remove_column}>Remove column</button>
-    </div>
-    <br>
-    <div><label>
-        Filter teams: 
-        <input type="text" bind:value={show}>
-    </label></div>
-    <label>
-        <input type="radio" name="positive" bind:group={positive} value={true}>
-        Include
-    </label>
-    <label>
-        <input type="radio" name="positive" bind:group={positive} value={false}>
-        Exclude
-    </label>    
-    <br><br>
-    <div>Filter matches:</div>
-    <label>
-        From:
-        <input class="w-24" type="number" bind:value={first}>
-    </label>
-    <label>
-        To:
-        <input class="w-24" type="number" bind:value={last}>
-    </label>    
-    <br><br>
-    <div class="flex flex-row items-center w-fit p-4">
-        <input type="checkbox" id="graph-switch" class="relative w-11 h-6 p-px bg-gray-100 border-transparent text-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:ring-blue-600 disabled:opacity-50 disabled:pointer-events-none checked:bg-none checked:text-blue-600 checked:border-blue-600 focus:checked:border-blue-600 dark:bg-gray-800 dark:border-gray-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-600 before:inline-block before:w-5 before:h-5 before:bg-white checked:before:bg-blue-200 before:translate-x-0 checked:before:translate-x-full before:rounded-full before:shadow before:transform before:ring-0 before:transition before:ease-in-out before:duration-200 dark:before:bg-gray-400 dark:checked:before:bg-blue-200"
-               on:click={() => showGraph = !showGraph}>
-        <label for="graph-switch" class="text-sm">Toggle Graphing</label>
-    </div>
-    {#if !showGraph}
-        <div class="flex flex-row w-fit gap-1">
-            <p>Sort:</p>
-            <select bind:value={sortFunction}>
-                {#each Object.keys(stats) as func}
-                    <option value={func}>{func}</option>
-                {/each}
-            </select>
+<div class="flex flex-col lg:flex-row justify-items-center">
+    <center class="pt-1 lg:basis-1/3">
+        <div class="grid grid-cols-2 w-fit gap-1">
+            <button class="font-bold bg-gradient-to-t from-green-800 to-green-400 border-green-900" on:click={add_column}>Add column</button>
+            <button class="font-bold bg-gradient-to-t from-red-800 to-red-400 border-red-900" on:click={remove_column}>Remove column</button>
         </div>
+        <br>
+        <div><label>
+            Filter teams:
+            <input type="text" bind:value={show}>
+        </label></div>
         <label>
-            <input type="radio" name="sort" bind:group={ascending} value={false}>
-            Descending
+            <input type="radio" name="positive" bind:group={positive} value={true}>
+            Include
         </label>
         <label>
-            <input type="radio" name="sort" bind:group={ascending} value={true}>
-            Ascending
+            <input type="radio" name="positive" bind:group={positive} value={false}>
+            Exclude
         </label>
-        <br>
-        <br>
-        <div class="box overflow-x-scroll max-w-full">
-            <table class="divide-y divide-white">
-                <tr>
-                    <th>#</th>
-                    {#each columns as col}
-                        <th>
-                            <select class="text-xs" style="max-width:80px" bind:value={col} on:change={update_graph_data}>
-                                {#each Object.keys(stats) as func}
-                                    <option value={func}>{func}</option>
-                                {/each}
-                            </select>
-                        </th>
-                    {/each}
-                </tr>
-
-                {#key first}{#key last}{#each teams as team, i (team)}
-                    <tr class="divide-x" animate:flip>
-                        {#if showteams?.[0]=="" || (showteams.includes(""+team) && positive) || (!showteams.includes(""+team) && !positive)}
-                            <td>{i+1}.</td>
-                            {#each columns as col}
-                                <td class:font-bold={col=="Team_number"}>{(typeof stats[col](team,data.entries.filter(matfil))==="number" && stats[col](team,data.entries.filter(matfil)) != Math.trunc(stats[col](team,data.entries.filter(matfil))))?parseFloat(stats[col](team,data.entries.filter(matfil))).toFixed(2):stats[col](team,data.entries.filter(matfil))}</td>
-                            {/each}
-                        {/if}
+        <br><br>
+        <div>Filter matches:</div>
+        <label>
+            From:
+            <input class="w-24" type="number" bind:value={first}>
+        </label>
+        <label>
+            To:
+            <input class="w-24" type="number" bind:value={last}>
+        </label>
+        <br><br>
+        <div class="flex flex-row items-center w-fit p-4">
+            <input type="checkbox" id="graph-switch" class="relative w-11 h-6 p-px bg-gray-100 border-transparent text-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:ring-blue-600 disabled:opacity-50 disabled:pointer-events-none checked:bg-none checked:text-blue-600 checked:border-blue-600 focus:checked:border-blue-600 dark:bg-gray-800 dark:border-gray-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-600 before:inline-block before:w-5 before:h-5 before:bg-white checked:before:bg-blue-200 before:translate-x-0 checked:before:translate-x-full before:rounded-full before:shadow before:transform before:ring-0 before:transition before:ease-in-out before:duration-200 dark:before:bg-gray-400 dark:checked:before:bg-blue-200"
+                   on:click={() => showGraph = !showGraph}>
+            <label for="graph-switch" class="text-sm">Toggle Graphing</label>
+        </div>
+        {#if !showGraph}
+            <button on:click={tableToCSV} class="font-bold bg-gradient-to-t from-teal-800  to-teal-400 border-black">Export sheet</button>
+        {/if}
+        <table>
+            {#if values}
+                {#each values as value}
+                    <tr>
+                        {#each value as subvalue}
+                            <th>{subvalue}</th>
+                        {/each}
                     </tr>
-                {/each}{/key}{/key}
-            </table>
-        </div>
-        <br>
-        <div class="opacity-50">*Score calculations do not include links</div>
-        <button on:click={tableToCSV} class="font-bold bg-gradient-to-t from-teal-800  to-teal-400 border-black">Export sheet</button>
-    {:else}
-        <div class="h-50 max-w-full">
-            X:
-            <select class="text-xs" style="max-width:120px" bind:value={columns[0]} on:change={update_graph_data}>
-                {#each Object.keys(stats) as func}
-                    <option value={func}>{func}</option>
                 {/each}
-            </select>
-            Y:
-            <select class="text-xs mb-3" style="max-width:120px" bind:value={columns[1]} on:change={update_graph_data}>
-                {#each Object.keys(stats) as func}
-                    <option value={func}>{func}</option>
-                {/each}
-            </select>
-            <Plot
-                    data={graph_data}
-                    layout={{showlegend: false,
+            {/if}
+        </table>
+    </center>
+    <div class="inline-block h-full min-h-[1em] w-0.5 self-stretch bg-neutral-100 opacity-100 dark:opacity-50"></div>
+    <center class="lg:basis-2/3">
+        {#if !showGraph}
+            <div class="flex flex-row w-fit gap-1">
+                <p>Sort:</p>
+                <select bind:value={sortFunction}>
+                    {#each Object.keys(stats) as func}
+                        <option value={func}>{func}</option>
+                    {/each}
+                </select>
+            </div>
+            <label>
+                <input type="radio" name="sort" bind:group={ascending} value={false} on:change={generate_new_data}>
+                Descending
+            </label>
+            <label>
+                <input type="radio" name="sort" bind:group={ascending} value={true} on:change={generate_new_data}>
+                Ascending
+            </label>
+            <br>
+            <br>
+            <div class="box overflow-x-scroll max-w-full">
+                <table class="divide-y divide-white">
+                    <tr>
+                        <th>#</th>
+                        {#each columns as col}
+                            <th>
+                                <select class="text-xs" style="max-width:80px" bind:value={col} on:change={update_graph_data}>
+                                    {#each Object.keys(stats) as func}
+                                        <option value={func}>{func}</option>
+                                    {/each}
+                                </select>
+                            </th>
+                        {/each}
+                    </tr>
+
+                    {#key first}{#key last}
+                    {#each filtered_teams as team, i (team)}
+                        <tr class="divide-x" animate:flip>
+                            {#if showteams?.[0]=="" || (showteams.includes(""+team) && positive) || (!showteams.includes(""+team) && !positive)}
+                                <td>{i+1}.</td>
+                                {#each columns as col, c}
+                                    <td class:font-bold={col=="Team_number"}>{filtered_data[c][i]}</td>
+                                {/each}
+                            {/if}
+                        </tr>
+                    {/each}{/key}{/key}
+                </table>
+            </div>
+            <br>
+            <p>{filtered_data}</p>
+            <Pagination rows={new_data} teams={teams} perPage={10} bind:trimmedRows={filtered_data} bind:trimmedTeams={filtered_teams}/>
+            <div class="opaczity-50">*Score calculations do not include links</div>
+        {:else}
+            <div class="h-50 max-w-full">
+                X:
+                <select class="text-xs" style="max-width:120px" bind:value={columns[0]} on:change={update_graph_data}>
+                    {#each Object.keys(stats) as func}
+                        <option value={func}>{func}</option>
+                    {/each}
+                </select>
+                Y:
+                <select class="text-xs mb-3" style="max-width:120px" bind:value={columns[1]} on:change={update_graph_data}>
+                    {#each Object.keys(stats) as func}
+                        <option value={func}>{func}</option>
+                    {/each}
+                </select>
+                <Plot
+                        data={graph_data}
+                        layout={{showlegend: false,
                              title: "Graphing " + columns[0] + " over " + columns[1],
                              margin: { t: 60, b: 40, l: 40, r: 30 },
                              xaxis: {showline: true, title: columns[0]},
                              yaxis: {title: columns[1]}}}
-                    fillParent=true
-                    debounce={250}
-            />
-        </div>
-    {/if}
-</center>
+                        fillParent=true
+                        debounce={250}
+                />
+            </div>
+        {/if}
+    </center>
+</div>
 
 <table bind:this={output} hidden>
     <tr>
