42a250e6ca6efc1062c0291deeb843141ef48c8f
Micah Lehman <micah.r.lehman@gmail.com>
2024-02-16 17:19:03 -0600

    stupid weird bugs fixed

--- src/routes/(authorized)/data/2023/entries/[event]/+page.server.js
+++ src/routes/(authorized)/data/2023/entries/[event]/+page.server.js
@@ -1,12 +1,12 @@
-import tba from "$lib/modules/tba";
+// import tba from "$lib/modules/tba";
 
-export async function load({params}){
-    const events = await tba(`events/2023`);
+// export async function load({params}){
+//     const events = await tba(`events/2023`);
 
-    events.push({
-        short_name: "Practice",
-        key: "2023practice"
-    })
+//     events.push({
+//         short_name: "Practice",
+//         key: "2023practice"
+//     })
 
-    return { events, competition:params.event };
-}
+//     return { events, competition:params.event };
+// }
