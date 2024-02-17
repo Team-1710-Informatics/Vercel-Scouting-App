aa2f29a1cf67f3fb3212c3721828fcc633fb56bc
Micah Lehman <micah.r.lehman@gmail.com>
2024-02-17 11:27:53 -0600

    paint + redirect easter egg

--- src/routes/(authorized)/paint/+page.svelte
+++ src/routes/(authorized)/paint/+page.svelte
@@ -1,14 +1,15 @@
 <script>
     import { onMount } from 'svelte';
     import ColorSwappingText from "$lib/components/visual/ColorSwappingText.svelte";
-    import { redirect } from '@sveltejs/kit';
 
     let canvas;
     let ctx;
     let active=false;
     let color = "#0e7cc1";
+    let link;
+    let started=false;
 
-    $: innerWidth = 0;
+    let innerWidth = 0;
 
     const sizes=[
         5,10,20,30,40
@@ -16,14 +17,16 @@
     
     let width = 5;
 
-    function clear(){
-        ctx.fillStyle="blue";
-        ctx.fillRect(0,0,innerWidth-50,500);
-    }
-
     onMount(()=>{
         ctx=canvas.getContext('2d');
     });
+
+    function clear(){
+        ctx.fillStyle="white";
+        ctx.fillRect(0,0,innerWidth-50,500);
+    }
+
+    // $: innerWidth, clear();
 
     function handleMouse(event){
         const rect = canvas.getBoundingClientRect();
@@ -40,6 +43,13 @@
         }
     }
 
+    function download(){
+        let image = canvas.toDataURL("image/png", 1.0).replace("image/png", "image/octet-stream");
+        var link = document.createElement('a');
+        link.download = "my-image.png";
+        link.href = image;
+        link.click();
+    }
 
 </script>
 
@@ -47,7 +57,10 @@
 
 <div class="flex flex-col overflow-x-hidden">
     <h1 class="text-5xl mx-auto underline my-5"><ColorSwappingText text={"SCOUTING PAINT"}/></h1>
-    <canvas width={innerWidth-50} height=400 class="mx-auto" bind:this={canvas} 
+    {#if !started}
+        <button class="w-fit mx-auto my-4 text-3xl border-2 rounded border-black bg-slate-600" on:click={()=>{started=true;clear();}}>Start Painting!</button>
+    {/if}
+    <canvas width={innerWidth-50} height=400 class="mx-auto border-2 border-black" bind:this={canvas} 
         on:touchstart={()=>{active=true;document.body.style.overflow = 'hidden';}} 
         on:touchend={()=>{active=false;document.body.style.overflow = 'auto';}} 
         on:mousedown={()=>{active=true;}} 
@@ -62,14 +75,12 @@
     </div>
     <input type="color" bind:value={color} class="mx-auto my-2"/>
     <button class="w-fit rounded border-2 mx-auto my-2" on:click={clear}>Clear/Reset</button>
-    <button class="w-fit rounded border-2 mx-auto my-2" on:click={()=>{
-        let image = ctx.toDataURL("image/png").replace("image/png", "image/octet-stream");
-        throw redirect(308, image);
-    }}>Download</button>
+    <button class="w-fit rounded border-2 mx-auto my-2" on:click={download}>Download</button>
+    <a bind:this={link}/>
 </div>
 
 <style>
     button:hover{
-        background-color:azure;
+        background-color:rgb(122, 139, 200);
     }
 </style>
