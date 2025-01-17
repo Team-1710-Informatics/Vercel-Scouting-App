23f4ffe741048e7d50d31c16647a951405a38cfe
Owen <owenschmidt2010@gmail.com>
2025-01-17 17:10:14 -0600

    better payout menu

--- src/routes/(authorized)/admin/payout/+page.server.js
+++ src/routes/(authorized)/admin/payout/+page.server.js
@@ -1,24 +1,78 @@
 import { User } from '$lib/server/models/index.js'
-
-//edit amount paid
-const payout = 2400
+import credits from '$lib/server/user/credi.ts'
 
 export const actions = {
-    default: async ({ request }) => {
-        const input = await request.formData()
-        const data = JSON.parse(input.get('data'))
+    default: async ({ request })=>{
+        const input = await request.formData();
+        const data = JSON.parse(input.get("data"));
 
-        const medias = await User.find({ permissions: 'media' })
-        const pits = await User.find({ permissions: 'pit' })
+        console.log(data.selected)
 
-        for (let i = 0; i < medias.length; i++) {
-            let user = medias[i]
-            let creds = user.credits
-            creds += payout
-            User.updateOne(
-                { username: user.username },
-                { $set: { credits: creds } }
-            )
+        const reason = "role payout";
+
+        let medias = [];
+
+        if (data.selected.includes('media')) {
+            console.log("media")
+            medias = await User.find({"permissions": "media"});
+            console.log(medias);
         }
-    },
-}
+
+        let pits = []
+
+        if (data.selected.includes('pit')) {
+            pits = await User.find({"permissions": "pit"});
+        }
+
+        let admins = [];
+
+        if (data.selected.includes('admin')) {
+            admins = await User.find({"permissions": "admin"});
+        }
+
+        let alreadyPaid = [];
+
+        let payout = data.amount[data.selected.indexOf("media")]
+        for (let i = 0; i < medias.length; i++){
+            let user = medias[i];
+            if (alreadyPaid.includes(user.username)) {
+                return
+            } else {
+                alreadyPaid.push(user.username);
+            }
+
+            let creds = user.credits
+            creds += payout;
+            await credits.transaction(user.username, creds, reason);
+        }
+
+        payout = data.amount[data.selected.indexOf("pit")]
+        for (let i = 0; i < pits.length; i++){
+            let user = pits[i];
+            if (alreadyPaid.includes(user.username)) {
+                return
+            } else {
+                alreadyPaid.push(user.username);
+            }
+
+            let creds = user.credits
+            creds += payout;
+            await credits.transaction(user.username, creds, reason);
+        }
+
+        payout = data.amount[data.selected.indexOf("admin")]
+        for (let i = 0; i < admins.length; i++){
+            let user = admins[i];
+            if (alreadyPaid.includes(user.username)) {
+                return
+            } else {
+                alreadyPaid.push(user.username);
+            }
+
+            let creds = user.credits
+            creds += payout;
+            await credits.transaction(user.username, creds, reason);
+        }
+
+    }
+};
--- src/routes/(authorized)/admin/payout/+page.svelte
+++ src/routes/(authorized)/admin/payout/+page.svelte
@@ -1,5 +1,53 @@
+<script type="text/javascript">
+    import Dropdown from '$lib/components/ui/Dropdown.svelte'
+
+    const roles = [
+        "media",
+        "admin",
+        "pit"
+    ]
+
+
+    $: selected = [];
+    $: amount = [];
+
+    let role = "";
+    let selectedAmount = 2400;
+
+    function addRole() {
+        selected.push(role);
+        selected = selected;
+        amount.push(selectedAmount);
+        amount = amount;
+        selectedAmount = 2400;
+    }
+
+    $: data = {
+        amount,
+        selected
+    }
+
+    $: output = JSON.stringify(data);
+
+</script>
+
 <middle>
+
+    <div class="box">
+        <select bind:value={role}>
+            {#each roles as p}
+                <option value={p}>{p}</option>
+            {/each}
+        </select>
+        <input type="number" defaultValue=2400 bind:value={selectedAmount} />
+        <button on:click={addRole}>Add</button>
+        {#each selected as p}
+            <p>{p} - {amount[selected.indexOf(p)]}</p>
+        {/each}
+    </div>
+
     <form method="POST">
-        <button class="submit">Payout 2400 Credits</button>
+        <input type="hidden" name="data" bind:value={output}/>
+        <button class="submit">Payout Credits</button>
     </form>
 </middle>
