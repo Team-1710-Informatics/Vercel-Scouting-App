14ef0fa103da00c70a85aab63341cc2f6d80b0b6
katee <kateeswails@gmail.com>
2025-01-17 15:52:55 -0600

    files move

--- src/routes/(authorized)/data/2025/AllianceSelection.svelte
+++ src/routes/(authorized)/data/2025/AllianceSelection.svelte
@@ -1,16 +1,57 @@
 <script>
     import tba from "$lib/modules/tba"; 
+    import { onMount } from 'svelte';
 
     export let event_key;
 
-    // $: rankings = ''
+    let rankings;
 
     async function getRankings() {
-        const rankings = await tba('event/' + event_key + '/rankings');
+        rankings = await tba('event/' + event_key + '/rankings');
         console.log(rankings);
     }
+
+    onMount(() => {
+        getRankings();
+    });
+
+
 </script>
 
-<div>
+<!-- <div>
     <button on:click={getRankings}>Test</button>
+    {#if rankings}
+        {#each rankings.rankings as ranking}
+            {ranking.team_key}
+        {/each}
+    {/if}
+</div> -->
+
+<div>
+    {#if rankings}
+        <div>{rankings.rankings[10].team_key}</div>
+    {/if}
 </div>
+
+<!-- <div>
+    <div on:mount={getRankings}>
+        {#if rankings}
+            {#each rankings.rankings as ranking}
+                {ranking.team_key}
+            {/each}
+        {/if}
+    </div>
+</div> -->
+
+
+<!-- <div flex flex-rows flex-wrap>
+    <div {getRankings}>
+        {#if rankings}
+            {#each rankings.rankings as ranking}
+                {ranking.team_key}
+            {/each}
+        {/if}
+    </div>
+    <div>Rankings</div>
+    <div>{rankings.team_key}</div>
+</div> -->
