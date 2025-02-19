67e10a40fe961864ebf23b0b33b10e7039c5e7a5
Sam <61994039+fourthDimensional@users.noreply.github.com>
2025-02-18 18:35:04 -0600

    Service Ping

---
+++ src/routes/(authorized)/services/ServicePing.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+<div class="flex flex-row items-center">
+    <div>1 | Match Prediction | match.apisb.me | Online</div>
+    <div class="w-2.5 h-2.5 bg-green-500 rounded-xl ml-4 mt-0.5"></div>
+</div>
