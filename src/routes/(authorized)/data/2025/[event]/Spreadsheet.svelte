4b9539fb4d4b543d568fb402428c3a06f2d49d04
ojsrb <66569940+ojsrb@users.noreply.github.com>
2025-02-21 17:10:34 -0600

    spreadsheet started

---
+++ src/routes/(authorized)/data/2025/[event]/Spreadsheet.svelte
@@ -0,0 +1,252 @@
+<script>
+    import { flip } from 'svelte/animate'
+    import stats from './statistics'
+
+    let teams = []
+    let first = 1
+    let last = 999
+
+    function matfil(e) {
+        return e.match >= first && e.match <= last
+    }
+
+    let show = ''
+    $: showteams = show.split(' ')
+
+    export let data
+    if (data) {
+        data.entries.forEach((e) => {
+            if (!teams.includes(e.team)) teams.push(e.team)
+        })
+    }
+
+    let columns = ['TeamNumber', 'AverageScore']
+    let sortFunction = 'AverageScore'
+    let ascending = false
+    let positive = true
+
+    let output
+
+    function tableToCSV() {
+        let csvdata = []
+
+        let rows = output.getElementsByTagName('tr')
+
+        for (let i = 0; i < rows.length; i++) {
+            let cols = rows[i].querySelectorAll('td,th')
+
+            let csvrow = []
+            for (let j = 0; j < cols.length; j++) {
+                csvrow.push(cols[j].innerHTML)
+            }
+            csvdata.push(csvrow.join(','))
+        }
+        csvdata = csvdata.join('\n')
+
+        downloadCSVFile(csvdata)
+    }
+
+    function downloadCSVFile(csvdata) {
+        let file = new Blob([csvdata], {
+            type: 'text/csv',
+        })
+
+        let temp = document.createElement('a')
+
+        temp.download = 'export.csv'
+        temp.href = window.URL.createObjectURL(file)
+        temp.style.display = 'none'
+        document.body.appendChild(temp)
+
+        temp.click()
+        document.body.removeChild(temp)
+    }
+
+    $: teams = teams.sort((a, b) => {
+        if (first && last) {
+        }
+        return (
+            (+stats[sortFunction](b, data.entries.filter(matfil)) -
+                +stats[sortFunction](a, data.entries.filter(matfil))) *
+            (ascending ? -1 : 1)
+        )
+    })
+</script>
+
+
+<center class="pt-10 px-10 max-w-full">
+    <div class="grid grid-cols-2 w-fit gap-1">
+
+    </div>
+    <br />
+    <div>
+        <table>
+            <tr class="outline outline-2 my-2">
+                <td class="outline outline-1">Filter teams:</td>
+                <td class="outline outline-1"><input type="text" bind:value={show} class="w-1/2"/></td>
+            </tr>
+            <tr class="outline outline-2">
+                <td class="w-1/2 outline outline-1">
+                    <input
+                    type="radio"
+                    name="positive"
+                    bind:group={positive}
+                    value={true}
+                    />
+                    Include
+                </td>
+                <td class="w-1/2 outline outline-1">
+                    <input
+                        type="radio"
+                        name="positive"
+                        bind:group={positive}
+                        value={false}
+                    />
+                    Exclude
+                </td>
+            </tr>
+            <tr class="outline outline-2 my-2">
+                <td class="outline outline-1">Matches:</td>
+                <td class="outline outline-1">
+                    From:
+                    <input class="w-10" type="number" bind:value={first} />
+                    To:
+                    <input class="w-10" type="number" bind:value={last} />
+                </td>
+            </tr>
+            <tr class="outline outline-2 my-2">
+                <td class="outline outline-1">Sort:</td>
+                <td class="outline outline-1">
+                    <select bind:value={sortFunction} class="w-1/2">
+                        {#each Object.keys(stats) as func}
+                            <option value={func}>{func}</option>
+                        {/each}
+                    </select>
+                </td>
+            </tr>
+            <tr class="outline outline-2 my-2">
+                <td class="w-1/2 outline outline-1">Ascending <input type="radio" name="sort" bind:group={ascending} value={true} /></td>
+                <td class="w-1/2 outline outline-1">Descending <input type="radio" name="sort" bind:group={ascending} value={false} /></td>
+            </tr>
+        </table>
+    </div>
+
+    <br />
+
+    <div class="outline outline-2 overflow-auto w-fit">
+        <table class="divide-y divide-white">
+            <tr>
+
+                <div class="flex flex-row">
+                    <button
+                        class="font-bold bg-gradient-to-t from-green-800 to-green-400 border-green-900 rounded-full w-8 h-8"
+                        on:click={() => {
+                    columns.push('AverageScore')
+                    columns = columns
+                }}>+</button
+                    >
+
+                    <button
+                        class="font-bold bg-gradient-to-t from-red-800 to-red-400 border-red-900 rounded-full w-8 h-8"
+                        on:click={() => {
+                    columns.pop('AverageScore')
+                    columns = columns
+                }}>-</button
+                    >
+                </div>
+
+                {#each columns as col}
+                    <th>
+                        <select
+                            class="text-xs"
+                            style="max-width:7.5rem"
+                            bind:value={col}
+                        >
+                            {#each Object.keys(stats) as func}
+                                <option value={func}>{func}</option>
+                            {/each}
+                        </select>
+                    </th>
+                {/each}
+            </tr>
+
+            {#key first}{#key last}{#each teams as team, i (team)}
+                <tr class="divide-x" animate:flip>
+                    {#if showteams?.[0] == '' || (showteams.includes('' + team) && positive) || (!showteams.includes('' + team) && !positive)}
+                        <td>{i + 1}.</td>
+                        {#each columns as col}
+                            <td class:font-bold={col == 'Team_number'}
+                            >{typeof stats[col](
+                                team,
+                                data.entries.filter(matfil)
+                            ) === 'number' &&
+                            stats[col](
+                                team,
+                                data.entries.filter(matfil)
+                            ) !=
+                            Math.trunc(
+                                stats[col](
+                                    team,
+                                    data.entries.filter(matfil)
+                                )
+                            )
+                                ? parseFloat(
+                                    stats[col](
+                                        team,
+                                        data.entries.filter(
+                                            matfil
+                                        )
+                                    )
+                                ).toFixed(2)
+                                : stats[col](
+                                    team,
+                                    data.entries.filter(matfil)
+                                )}</td
+                            >
+                        {/each}
+                    {/if}
+                </tr>
+            {/each}{/key}{/key}
+        </table>
+    </div>
+    <br />
+    <div class="opacity-50">*Score calculations do not include links</div>
+    <button
+        on:click={tableToCSV}
+        class="font-bold bg-gradient-to-t from-teal-800 to-teal-400 border-black"
+    >Export sheet</button
+    >
+</center>
+
+<table bind:this={output} hidden>
+    <tr>
+        {#each columns as col}
+            <th>
+                {col}
+            </th>
+        {/each}
+    </tr>
+
+    {#each teams as team}
+        <tr class="divide-x">
+            {#each columns as col}
+                <td>{stats[col](team, data.entries)}</td>
+            {/each}
+        </tr>
+    {/each}
+</table>
+
+<style>
+    th,
+    td,
+    table {
+        padding: 4px;
+        text-align: center;
+        border-radius: 4px;
+    }
+
+    select {
+        padding: 2px;
+        border-radius: 4px;
+    }
+</style>
