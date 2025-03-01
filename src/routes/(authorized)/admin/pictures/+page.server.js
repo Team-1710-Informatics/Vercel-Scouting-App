6b4c2459f256f3266106951729b28de614ccaa73
ojsrb <66569940+ojsrb@users.noreply.github.com>
2025-03-01 15:47:13 -0600

    remade pit scouting pictures ui (its better now)

--- src/routes/(authorized)/admin/pictures/+page.server.js
+++ src/routes/(authorized)/admin/pictures/+page.server.js
@@ -1,5 +1,7 @@
 export async function load({ locals }) {
+    // url for AppScript Web App, returns objects with team, photo link, and scout
     const url = "https://script.google.com/macros/s/AKfycbwOUHmCNgmQVijp05G8pW2PJ0XvvOlQlHV8L_2jmI--zeqW-nON0ALyg3ChWMIwV7bO9A/exec"
+
     let response = await fetch(url)
     let data = await response.json()
     return data
--- src/routes/(authorized)/admin/pictures/+page.svelte
+++ src/routes/(authorized)/admin/pictures/+page.svelte
@@ -1,14 +1,18 @@
 <script>
     export let data;
 
-    let teamSearch = 1710
+    $: teamSearch = 1710
 
     let entries = data.data
 
     let teams = []
 
+    let showAll = false
+
     for (let i = 0; i < entries.length; i++) {
-        teams.push(entries[i].team)
+        if (!teams.includes(entries[i].team)) {
+            teams.push(entries[i].team)
+        }
     }
 
 </script>
@@ -16,25 +20,38 @@
 <center>
 
     <div class="flex flex-row h-fit mx-auto w-fit">
-        <p class="align-middle my-auto mr-2">Team Number</p>
-        <select bind:value={teamSearch} class="my-2 w-20 h-7" >
-            {#each teams as team}
-                <option value={team} >{team}</option>
-            {/each}
-        </select>
+        <p class="align-middle my-auto mr-2">Team Number: </p>
         <input type="number" bind:value={teamSearch} class="my-2 w-16 h-7"/>
+    </div>
+    <hr class="my-2 mb-2 w-1/2" />
+    <div class="w-1/2 flex flex-wrap items-center">
+        <button on:click={() => {showAll = true}} class="underline font-bold">Show All</button>
+        {#each teams as a}
+            <button on:click={() => {teamSearch = a; showAll = false}} class="underline">{a}</button>
+        {/each}
     </div>
 
     <hr class="my-2 mb-2 w-1/2" />
     <br />
+    <div class="w-5/6 flex flex-wrap mx-auto">
+        {#each data.data as entry}
+            {#if entry.team === teamSearch && !showAll}
+                <div class="flex flex-col mb-3">
+                    <p class="text-sm">Scout: {entry.scout}</p>
+                    <iframe src={entry.picture} allow="autoplay" width="600" height="400" class="mx-auto" title="beep boop"></iframe>
+                </div>
+            {/if}
+            {#if showAll && teams.includes(entry.team)}
+                <div class="flex flex-col mb-3">
+                    <p class="text-md">{entry.team}</p>
+                    <iframe src={entry.picture} allow="autoplay" width="600" height="400" class="mx-auto" title="beep boop"></iframe>
+                </div>
+            {/if}
+        {/each}
+        {#if teams.includes(teamSearch) !== true && !showAll}
+            <p class="mx-auto w-fit">No available pictures.</p>
 
-    {#each data.data as entry}
-        {#if entry.team == teamSearch}
-            <div class="flex flex-col mb-3">
-                <p class="text-sm">Scout: {entry.scout}</p>
-                <iframe src={entry.picture} allow="autoplay" width="600" height="400" class="mx-auto" title="beep boop"></iframe>
-            </div>
         {/if}
-    {/each}
+    </div>
 
 </center>
