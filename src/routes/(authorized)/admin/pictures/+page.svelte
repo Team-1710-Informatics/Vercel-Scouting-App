bfe07615cf92e87009a031e1584b9d41ec99e0bc
ojsrb <66569940+ojsrb@users.noreply.github.com>
2025-03-01 21:54:51 -0600

    adjustable picture size

--- src/routes/(authorized)/admin/pictures/+page.svelte
+++ src/routes/(authorized)/admin/pictures/+page.svelte
@@ -15,13 +15,21 @@
         }
     }
 
+    let imgWidth = 32.33
+
 </script>
 
-<center>
+<center class="w-fit">
 
     <div class="flex flex-row h-fit mx-auto w-fit">
-        <p class="align-middle my-auto mr-2">Team Number: </p>
-        <input type="number" bind:value={teamSearch} class="my-2 w-16 h-7"/>
+        <p class="align-middle my-auto mr-2">Team Number</p>
+        <input type="number" bind:value={teamSearch} class="my-2 w-16 h-7 mr-16"/>
+        <p class="align-middle my-auto mr-2">Columns</p>
+        <select class="my-2 w-16 h-7 mr-16" bind:value={imgWidth}>
+            <option value="32.33">3</option>
+            <option value="48.7">2</option>
+            <option value="100">1</option>
+        </select>
     </div>
     <hr class="my-2 mb-2 w-1/2" />
     <div class="w-1/2 flex flex-wrap items-center">
@@ -33,24 +41,23 @@
 
     <hr class="my-2 mb-2 w-1/2" />
     <br />
-    <div class="w-5/6 flex flex-wrap mx-auto">
+    <div class="flex flex-wrap mx-auto w-screen">
         {#each data.data as entry}
             {#if entry.team === teamSearch && !showAll}
-                <div class="flex flex-col mb-3">
+                <div class="flex flex-col m-2" style="width: {imgWidth}%; height: {imgWidth / 1.5}%;}">
                     <p class="text-sm">Scout: {entry.scout}</p>
-                    <iframe src={entry.picture} allow="autoplay" width="600" height="400" class="mx-auto" title="beep boop"></iframe>
+                    <iframe src={entry.picture} allow="autoplay" class="mx-auto w-full" height={imgWidth * 10} title="beep boop"></iframe>
                 </div>
             {/if}
             {#if showAll && teams.includes(entry.team)}
-                <div class="flex flex-col mb-3">
-                    <p class="text-md">{entry.team}</p>
-                    <iframe src={entry.picture} allow="autoplay" width="600" height="400" class="mx-auto" title="beep boop"></iframe>
+                <div class="flex flex-col m-2" style="width: {imgWidth}%; height: {imgWidth / 1.5}%;}">
+                    <p class="text-md font-bold">{entry.team}</p>
+                    <iframe src={entry.picture} allow="autoplay" height={imgWidth * 10} class="mx-auto w-full" title="beep boop"></iframe>
                 </div>
             {/if}
         {/each}
         {#if teams.includes(teamSearch) !== true && !showAll}
-            <p class="mx-auto w-fit">No available pictures.</p>
-
+            <p class="">No available pictures.</p>
         {/if}
     </div>
 
