6ac9116aa8ad1af1ec6f11d7763458565d2597b8
Sam <finepointdog@gmail.com>
2025-03-05 21:04:03 -0600

    change barge to score

--- src/routes/(authorized)/scout/2025/Barge.svelte
+++ src/routes/(authorized)/scout/2025/Barge.svelte
@@ -11,7 +11,7 @@
 
     function select(location) {
         if (!state.started) return
-        selected = { location: '' }
+        selected = {location: ''}
         selected.location = location
         item = 'algae'
 
@@ -20,7 +20,7 @@
 
             log.push({
                 time: state.time,
-                action: 'intake',
+                action: 'score',
                 ...selected,
                 phase: state.phase,
                 item: item,
@@ -31,22 +31,22 @@
 
 <div class="flex h-full basis-1/6 {flip ? 'flex-col' : 'flex-col-reverse'}">
     <button
-        class="basis-1/4 w-full bg-slate-600 text-center hover:brightness-110 {flip
+            class="basis-1/4 w-full bg-slate-600 text-center hover:brightness-110 {flip
             ? 'rounded-tl-3xl'
             : 'rounded-br-3xl'}"
-        class:brightness-120={selected.location === 'processor'}
-        on:click={() => {
+            class:brightness-120={selected.location === 'processor'}
+            on:click={() => {
             select('processor')
         }}
     >
         PROCESSOR
     </button>
     <button
-        class="basis-3/4 w-full bg-slate-500 text-center hover:brightness-110 {flip
+            class="basis-3/4 w-full bg-slate-500 text-center hover:brightness-110 {flip
             ? 'rounded-bl-3xl'
             : 'rounded-tr-3xl'} vertical-text"
-        class:brightness-120={selected.location === 'barge'}
-        on:click={() => {
+            class:brightness-120={selected.location === 'barge'}
+            on:click={() => {
             select('barge')
         }}
     >
