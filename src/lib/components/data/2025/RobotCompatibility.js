6c71d87a3bec33a5a70cd355c4dfb8a9d4b1d334
Sam <finepointdog@gmail.com>
2025-03-07 13:52:04 -0600

    updates

--- src/lib/components/data/2025/RobotCompatibility.js
+++ src/lib/components/data/2025/RobotCompatibility.js
@@ -1,12 +1,12 @@
 export const ApiService = {
     async request(route) {
-        // let endpoint = 'https://micro.apisb.me'
-        // return await fetch(endpoint + route)
+        let endpoint = 'https://micro.apisb.me'
+        let data = await fetch(endpoint + route)
 
-        let data = await fetch('http://127.0.0.1:5000/number')
+        // let data = await fetch('https://micro.apisb.me/')
 
         let compatibility = await data.json()
 
         return { compatibility: await compatibility.compatibility }
-    }
+    },
 }
