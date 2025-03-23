207b2be48db4e6e5cfa6df4d53b83afb478bae07
ojsrb <66569940+ojsrb@users.noreply.github.com>
2025-03-23 17:43:57 -0500

    updated deployment link for pit scout pictures

--- src/routes/(authorized)/admin/pictures/+page.server.js
+++ src/routes/(authorized)/admin/pictures/+page.server.js
@@ -1,8 +1,10 @@
 export async function load({ locals }) {
     // url for AppScript Web App, returns objects with team, photo link, and scout
-    const url = "https://script.google.com/macros/s/AKfycbwOUHmCNgmQVijp05G8pW2PJ0XvvOlQlHV8L_2jmI--zeqW-nON0ALyg3ChWMIwV7bO9A/exec"
+    const url = "https://script.google.com/macros/s/AKfycbyT3jFLqPRAtGlseIF2Dz8fZlG-YwsKjUyibDjKa_PSLMt4u3O0pURhufsYcSqERvDD5A/exec"
 
     let response = await fetch(url)
+    console.log(response)
     let data = await response.json()
+
     return data
 }
