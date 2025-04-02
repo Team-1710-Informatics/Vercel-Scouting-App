032051505d1b834ec5ba595d3eab890825acc224
Sam <61994039+fourthDimensional@users.noreply.github.com>
2025-04-02 18:35:35 -0500

    scouting stats

--- src/routes/(authorized)/admin/stats/+page.server.js
+++ src/routes/(authorized)/admin/stats/+page.server.js
@@ -11,19 +11,31 @@ export async function load({ locals }) {
         events.push(event.match.split('_')[0])
     })
 
+    events = [...new Set(events)]
+
+    let scouts = []
     let output = []
     for (const event of events) {
         let eventTeamSelections = await TeamSelections.find({
-            $regex: event + '.*',
+            match: { $regex: event + '*' },
         })
+        console.log('Event #: ' + eventTeamSelections.length)
         let matchCount = 0
         let totalScouts = 0
+        let missingScouts = 0
+        let bonus = 0
 
         matchCount = eventTeamSelections.length
 
         eventTeamSelections.forEach((match) => {
+            let fill = 0
             match.teams.forEach((team) => {
-                if (team.scout) totalScouts += team.scout.length
+                if (team.scout) {
+                    totalScouts += team.scout.length
+                    fill++
+                    if (fill === 6) bonus++
+                    scouts.push(team.scout)
+                } else missingScouts++
             })
         })
 
@@ -33,13 +45,23 @@ export async function load({ locals }) {
         let scoutingCoverage = totalScouts / (matchCount * 6)
         console.log(scoutingCoverage)
 
+        scouts = scouts.flat()
+        scouts = [...new Set(scouts)]
+
         output.push({
             event: event,
             matches: matchCount,
+            uniqueScouts: scouts.length,
+            missingScouts: missingScouts,
             scouts: totalScouts,
             coverage: scoutingCoverage,
+            bonus: bonus,
         })
+
+        matchCount = 0
+        totalScouts = 0
+        scouts = []
     }
 
-    return { output }
+    return { event: output }
 }
