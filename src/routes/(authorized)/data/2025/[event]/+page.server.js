1b434046c6f59faa15bb7aec0b4622e4ba2af949
ojsrb <66569940+ojsrb@users.noreply.github.com>
2025-04-04 15:46:01 -0500

    seed prediction

--- src/routes/(authorized)/data/2025/[event]/+page.server.js
+++ src/routes/(authorized)/data/2025/[event]/+page.server.js
@@ -1,5 +1,7 @@
 import { ScoutData } from '$lib/server/models'
 import { pitdata2025 } from '$lib/server/models'
+import tba from '$lib/modules/tba.js'
+import { seedPredictor } from '$lib/server/models/seedPredictor.js'
 
 export async function load({ params }) {
     const entries = JSON.parse(
@@ -11,6 +13,8 @@ export async function load({ params }) {
     )
 
     console.log(pitData)
+
+    console.log(seedPredictor(params.event));
 
     let data = {
         event: params.event,
