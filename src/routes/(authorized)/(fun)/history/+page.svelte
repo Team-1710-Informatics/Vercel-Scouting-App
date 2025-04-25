f3d85f291300709fcbedd89f971610731f879840
Sam <finepointdog@gmail.com>
2025-04-24 19:11:16 -0500

    Graveyard 4.

--- src/routes/(authorized)/(fun)/history/+page.svelte
+++ src/routes/(authorized)/(fun)/history/+page.svelte
@@ -1,13 +1,13 @@
 <div class="w-full h-full flex items-center justify-center flex-col text-left m-10 gap-2">
     <div class="text-left">
-        RIP Charlie Bergdall (Founder of Informatics) (2021-2022);
-        RIP Zane Zollers (2021-2022);
-        RIP Gavin Behrends (Leader) (2021-2023);
-        RIP Hope Wagner (2022-2023);
-        RIP Micah Lehman (Leader) (2022-2024);
-        RIP Kate Swails (2022-2025);
-        Ben Garcia (2023-Current);
-        Sam Bargallo (Leader) (2023-Current);
+        RIP Charlie Bergdall (Founder of Informatics) (2021-2022);<br>
+        RIP Zane Zollers (2021-2022);<br>
+        RIP Gavin Behrends (Leader) (2021-2023);<br>
+        RIP Hope Wagner (2022-2023);<br>
+        RIP Micah Lehman (Leader) (2022-2024);<br>
+        RIP Kate Swails (2022-2025);<br>
+        Ben Garcia (2023-Current);<br>
+        Sam Bargallo (Leader) (2023-Current);<br>
     </div>
     <div>
         Owen Schmidt (2024-Current);
