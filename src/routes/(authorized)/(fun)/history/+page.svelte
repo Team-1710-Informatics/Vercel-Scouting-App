52afff49409d81351517bb4de117b827d1f7bb79
Sam <finepointdog@gmail.com>
2025-04-24 19:14:04 -0500

    Graveyard 5.

--- src/routes/(authorized)/(fun)/history/+page.svelte
+++ src/routes/(authorized)/(fun)/history/+page.svelte
@@ -1,4 +1,7 @@
 <div class="w-full h-full flex items-center justify-center flex-col text-left m-10 gap-2">
+    <h1 class="m-4 text-3xl font-bold">
+        Informatics Graveyard
+    </h1>
     <div class="text-left">
         RIP Charlie Bergdall (Founder of Informatics) (2021-2022);<br>
         RIP Zane Zollers (2021-2022);<br>
@@ -8,8 +11,6 @@
         RIP Kate Swails (2022-2025);<br>
         Ben Garcia (2023-Current);<br>
         Sam Bargallo (Leader) (2023-Current);<br>
-    </div>
-    <div>
         Owen Schmidt (2024-Current);
     </div>
 </div>
